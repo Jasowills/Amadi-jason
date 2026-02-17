aca342f91b028eb863d148c22f22e7d4f0e761f1
Jason Amadi <Jason.amadi@marklite.com>
2026-02-17 17:04:39 +0100

    refactor: improve formatting and structure in Projects component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -54,89 +54,98 @@ const Projects = ({ active }) => {
     }, [filter]);
 
     return (
-        <article className={`page ${active ? "active" : ""}`} data-page="Projects">
-            <header>
-                <h2 className="h2 article-title">Projects</h2>
-            </header>
+      <article
+        className={`page ${active ? "active" : ""}`}
+        data-page="Projects"
+      >
+        <header>
+          <h2 className="h2 article-title">Projects</h2>
+        </header>
 
-            <section className="projects">
-                <div className="filter-select-box">
-                    <button className="filter-select" data-select ref={selectRef}>
-                        <div className="select-value" data-select-value ref={selectValueRef}>
-                            Select category
-                        </div>
-                        <div className="select-icon">
-                            <ion-icon name="chevron-down"></ion-icon>
-                        </div>
-                    </button>
+        <section className="projects">
+          <div className="filter-select-box">
+            <button className="filter-select" data-select ref={selectRef}>
+              <div
+                className="select-value"
+                data-select-value
+                ref={selectValueRef}
+              >
+                Select category
+              </div>
+              <div className="select-icon">
+                <ion-icon name="chevron-down"></ion-icon>
+              </div>
+            </button>
+          </div>
+
+          <ul className="service-list">
+            {[
+              {
+                title: "Resortify",
+                category: "API",
+                description:
+                  "A comprehensive hotel reservation system built as a capstone project for the Thinkful curriculum. Resortify provides a complete solution for managing hotel bookings, room availability, and customer reservations.",
+                link: "https://github.com/Jasowills/Resortify-hotel-reservation-system",
+              },
+              {
+                title: "MedPay Tracka Backend",
+                category: "API",
+                description:
+                  " A comprehensive financial tracking backend API that enables users to manage their income and expenses with detailed analytics and insights. Built for personal finance management and expense tracking applications.",
+                link: "hhttps://github.com/Jasowills/medpay-tracka-backend",
+              },
+              {
+                title: "Wayfarer - Backend ",
+                category: "API",
+                description:
+                  "Wayfarer Backend is a server-side application for a public bus transportation booking system. The system serves as a backend API that enables users to book bus trips conveniently from anywhere hotel-management-system · GitHub Topics · GitHub +2, making public transportation more accessible and user-friendly.",
+                link: "https://github.com/Jasowills/Wayfarer-backend",
+              },
+              {
+                title: "Neuron - meet",
+                category: "UI",
+                description:
+                  "Real-time video conferencing with WebRTC, React, and NestJS.",
+                link: "https://github.com/Jasowills/Neuron-meet",
+              },
+            ].map((project, index) => (
+              <li
+                key={index}
+                className="service-item"
+                data-filter-item
+                data-category={project.category}
+                ref={(el) => (filterItemsRef.current[index] = el)}
+              >
+                <div className="service-icon-box">
+                  <IoLogoGithub
+                    style={{ color: "hsl(123, 100%, 72%)", fontSize: "24px" }}
+                  />
                 </div>
-
-                <ul className="service-list">
-                    {[
-                        {
-                            title: "Resortify",
-                            category: "API",
-                            description:
-                                "A comprehensive hotel reservation system built as a capstone project for the Thinkful curriculum. Resortify provides a complete solution for managing hotel bookings, room availability, and customer reservations.",
-                            link: "https://github.com/Jasowills/Resortify-hotel-reservation-system",
-                        },
-                        {
-                            title: "MedPay Tracka Backend",
-                            category: "API",
-                            description:
-                                " A comprehensive financial tracking backend API that enables users to manage their income and expenses with detailed analytics and insights. Built for personal finance management and expense tracking applications.",
-                            link: "hhttps://github.com/Jasowills/medpay-tracka-backend",
-                        },
-                        {
-                            title: "Wayfarer - Backend ",
-                            category: "API",
-                            description:
-                                "Wayfarer Backend is a server-side application for a public bus transportation booking system. The system serves as a backend API that enables users to book bus trips conveniently from anywhere hotel-management-system · GitHub Topics · GitHub +2, making public transportation more accessible and user-friendly.",
-                            link: "https://github.com/Jasowills/Wayfarer-backend",
-                        },
-                        {
-                            title: "Neuron - Resume Builder",
-                            category: "UI",
-                            description:
-                                "A modern, intuitive web application that empowers users to create professional resumes effortlessly. Built with React.js,",
-                            link: "https://github.com/Jasowills/Neuron-s-Resume-builder",
-                        },
-                    ].map((project, index) => (
-                        <li
-                            key={index}
-                            className="service-item"
-                            data-filter-item
-                            data-category={project.category}
-                            ref={(el) => (filterItemsRef.current[index] = el)}
-                        >
-                            <div className="service-icon-box">
-                                <IoLogoGithub style={{ color: "hsl(123, 100%, 72%)", fontSize: "24px" }} />
-                            </div>
-                            <div className="service-content-box">
-                                <h3 className="service-item-title h4">{project.title}</h3>
-                                <p className="service-item-text">{project.description}</p>
-                                <br />
-                                <div className="project-links">
-                                    <a
-                                        href={project.link}
-                                        target="_blank"
-                                        rel="noopener noreferrer"
-                                        className="project-link"
-                                    >
-                                        View Project
-                                    </a>
-                                </div>
-                            </div>
-                        </li>
-                    ))}
-                </ul>
-                <ul className="filter-list">
-                    <a href="https://github.com/jasowills">
-                        <li className="filter-item">View More</li>
+                <div className="service-content-box">
+                  <h3 className="service-item-title h4">{project.title}</h3>
+                  <p className="service-item-text">{project.description}</p>
+                  <br />
+                  <div className="project-links">
+                    <a
+                      href={project.link}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="project-link"
+                    >
+                      View Project
                     </a>
-                </ul>
-            </section>
-        </article>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+          <ul className="filter-list">
+            <a href="https://github.com/jasowills">
+              <li className="filter-item">View More</li>
+            </a>
+          </ul>
+        </section>
+      </article>
     );
 };
 
