f01aae667c88f241bd45e1ef797b9ec0df414798
Jason Amadi <jujuzhaddy01@gmail.com>
2026-07-21 21:58:29 +0100

    enable session recording in sonar sdk

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,7 @@ try {
     window.__sonar = new SonarWeb({
       apiKey: 'wdp_1109979e_913bea168dbba1e3b502664503d83a33fc824c538d51b61a',
       autoTrack: true,
+      sessionRecording: true,
     })
   })
 } catch {
